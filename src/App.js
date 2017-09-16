0e887d7cc7b223a56443c3eef71d41157e8e014c
Glenn Reyes <glenn@glennreyes.com>
2017-09-16 17:23:21 +0200

    Create a simple example

--- src/App.js
+++ src/App.js
@@ -1,74 +1,21 @@
 import React, { Component } from 'react';
-import Loadable from 'react-loadable';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Loading from './Loading';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   state = {
-    AsyncComponent: () => (
-      <span onMouseEnter={this.onMouseOver}>
-        Hover here to load async content
-      </span>
-    ),
+    AsyncComponent: () => <div>Nothing loaded</div>,
   };
 
-  onMouseOver = async () => {
-    const module = await import(/* webpackChunkName: "async" */ './AsyncComponent');
+  async componentDidMount() {
+    const module = await import('./AsyncComponent');
     const AsyncComponent = module.default;
 
     this.setState({ AsyncComponent });
-  };
+  }
 
   render() {
     const { AsyncComponent } = this.state;
 
-    return (
-      <Router
-        basename={
-          process.env.NODE_ENV === 'production'
-            ? '/code-splitting-example'
-            : '/'
-        }
-      >
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Code Splitting example</h2>
-            <nav className="Nav">
-              <Link className="Link" to="/">
-                Home
-              </Link>
-              <Link className="Link" to="/profile">
-                Profile
-              </Link>
-            </nav>
-          </div>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              component={Loadable({
-                loader: () => import(/* webpackChunkName: "home" */ './Home'),
-                loading: Loading,
-              })}
-            />
-            <Route
-              path="/profile"
-              component={Loadable({
-                loader: () =>
-                  import(/* webpackChunkName: "profile" */ './Profile'),
-                loading: Loading,
-              })}
-            />
-          </Switch>
-          <footer>
-            <AsyncComponent />
-          </footer>
-        </div>
-      </Router>
-    );
+    return <AsyncComponent />;
   }
 }
 
